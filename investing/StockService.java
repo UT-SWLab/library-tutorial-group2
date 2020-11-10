a978dc161e44bcb8f8f6e026ab1fb7c0755e3c14
Kvnmedina <31513306+Kvnmedina@users.noreply.github.com>
2020-11-10 09:38:47 -0600

    Delete StockService.java

--- investing/StockService.java
+++
@@ -1,8 +0,0 @@
-package investing;
-import java.util.*;
-
-public interface StockService {
-    public double getPrice(Stock stock);
-    public double getBeta(Stock stock);
-    public double getDividend(Stock stock);
-}
